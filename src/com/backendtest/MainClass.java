7478e81e12e924ea758d085a460c400f1737851d
ibnahmadbello <justforlearningcode@gmail.com>
2021-12-28 23:41:34 +0100

    Display phone Numbers

--- src/com/backendtest/MainClass.java
+++ src/com/backendtest/MainClass.java
@@ -14,18 +14,21 @@ public class MainClass {
 	public static void main(String[] args) throws Exception {
 		scanner = new Scanner(System.in);
 		phoneNumbers = new ArrayList<PhoneNumber>();
-		System.out.println("*****Phone Book*****");
-		System.out.println("Please reply with the correct option:");
-		System.out.println("1. To add a phone number\n2. To search for a number\n3. To see list of phone numbers");
+		displayMenu();
 
 		int userResponse = scanner.nextInt();
 		switch (userResponse) {
+		case 0:
+			System.exit(0);
+			break;
 		case 1:
-			addNumber();
+			addPhoneNumber();
 			break;
 		case 2:
+			searchPhoneNumber();
 			break;
 		case 3:
+			displayPhoneNumber();
 			break;
 		default:
 			break;
@@ -36,14 +39,29 @@ public class MainClass {
 //		
 //		PhoneNumber $090 = new PhoneNumber("Smith", "xyz", new Date());
 //		numbers.add($090);
-//		
-//		for (PhoneNumber phoneNumber : numbers) {
-//			System.out.print(phoneNumber.getName() + " " + phoneNumber.getEmail() + " " + phoneNumber.getDateCreated());
-//			System.out.println();
-//		}
 	}
 
-	private static void addNumber() throws Exception {
+	private static void displayPhoneNumber() {
+		// TODO Auto-generated method stub
+		for (PhoneNumber phoneNumber : phoneNumbers) {
+			System.out.printf("%10s%30s\n", "Name:", phoneNumber.getName());
+			System.out.printf("%10s%30s\n", "Email:", phoneNumber.getEmail());
+			System.out.printf("%10s%30s\n", "Date:", phoneNumber.getDateCreated());
+		}
+	}
+
+	private static void searchPhoneNumber() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void displayMenu() {
+		System.out.println("*****Phone Book*****");
+		System.out.println("Please reply with the correct option:");
+		System.out.println("0. To quit/exit the phone book.\n1. To add a phone number\n2. To search for a number\n3. To see list of phone numbers");
+	}
+
+	private static void addPhoneNumber() throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Enter name:");
 		String name = scanner.next();
@@ -58,15 +76,16 @@ public class MainClass {
 		}
 		System.out.println("Do you want to add another number? yes or no(y/n)?");
 		String answer = scanner.next();
-		if (answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no") || answer.equalsIgnoreCase("y")
-				|| answer.equalsIgnoreCase("n")) {
-			addNumber();
+		if (answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("y")) {
+			addPhoneNumber();
+		} else {
+			displayMenu();
 		}
 	}
 
 	private static boolean checkForUniqueness(String name, String email) {
 		// TODO Auto-generated method stub
-		boolean status = false;
+		boolean status = true;
 		for (PhoneNumber phoneNumber : phoneNumbers) {
 			if(phoneNumber.getName().equalsIgnoreCase(name) || phoneNumber.getEmail().equalsIgnoreCase(email)) {
 				status = false;
