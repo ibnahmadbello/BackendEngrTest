2f43e6765ce6d66ea6fdc4b6b96a45ff3eb4ace6
ibnahmadbello <justforlearningcode@gmail.com>
2021-12-28 23:15:47 +0100

    Check for uniqueness to add phone Number

--- src/com/backendtest/MainClass.java
+++ src/com/backendtest/MainClass.java
@@ -10,16 +10,14 @@ public class MainClass {
 
 	private static Scanner scanner;
 	private static ArrayList<PhoneNumber> phoneNumbers;
-	
-	
-	
+
 	public static void main(String[] args) throws Exception {
 		scanner = new Scanner(System.in);
 		phoneNumbers = new ArrayList<PhoneNumber>();
 		System.out.println("*****Phone Book*****");
 		System.out.println("Please reply with the correct option:");
 		System.out.println("1. To add a phone number\n2. To search for a number\n3. To see list of phone numbers");
-		
+
 		int userResponse = scanner.nextInt();
 		switch (userResponse) {
 		case 1:
@@ -32,8 +30,7 @@ public class MainClass {
 		default:
 			break;
 		}
-		
-		
+
 //		PhoneNumber $080 = new PhoneNumber("Jame", "abc", new Date());
 //		numbers.add($080);
 //		
@@ -52,9 +49,33 @@ public class MainClass {
 		String name = scanner.next();
 		System.out.println("Enter Email:");
 		String email = scanner.next();
-		PhoneNumber number = new PhoneNumber(name, email, new Date());
-		phoneNumbers.add(number);
-		System.out.println("Phone Number added successfully.");
+		if (checkForUniqueness(name, email)) {
+			PhoneNumber number = new PhoneNumber(name, email, new Date());
+			phoneNumbers.add(number);
+			System.out.println("Phone Number added successfully.");
+		} else {
+			System.out.println("Phone Number already exist.");
+		}
+		System.out.println("Do you want to add another number? yes or no(y/n)?");
+		String answer = scanner.next();
+		if (answer.equalsIgnoreCase("yes") || answer.equalsIgnoreCase("no") || answer.equalsIgnoreCase("y")
+				|| answer.equalsIgnoreCase("n")) {
+			addNumber();
+		}
+	}
+
+	private static boolean checkForUniqueness(String name, String email) {
+		// TODO Auto-generated method stub
+		boolean status = false;
+		for (PhoneNumber phoneNumber : phoneNumbers) {
+			if(phoneNumber.getName().equalsIgnoreCase(name) || phoneNumber.getEmail().equalsIgnoreCase(email)) {
+				status = false;
+			} else {
+				status = true;
+			}
+		}
+		return status;
+	
 	}
 
 }
